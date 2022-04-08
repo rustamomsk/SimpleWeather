e85d50f073e6897eb1ed171abf3028b1ecd5da11
rustamomsk <87946097+rustamomsk@users.noreply.github.com>
2022-04-09 02:04:38 +0300

    Update settings.py

--- WeatherApp/WeatherApp/settings.py
+++ WeatherApp/WeatherApp/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-**5^$*!jk3999wd#s2jy6_639n26q_)3*=1%1ku=mdi5tjzkt-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
